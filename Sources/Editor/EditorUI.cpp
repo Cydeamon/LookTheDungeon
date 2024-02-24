5ad2cd9ff061da8253a9459bf45e6cce4cb62959
Cydeamon <loknhess@gmail.com>
2024-02-24 18:36:36 +0300

    Editor UI basic layout

--- Sources/Editor/EditorUI.cpp
+++ Sources/Editor/EditorUI.cpp
@@ -1,4 +1,14 @@
 #include "EditorUI.h"
+#include <windows.h>
+
+EditorUI::EditorUI()
+{
+    ImGui::StyleColorsDark();
+}
+
+EditorUI::~EditorUI()
+{
+}
 
 EditorUI &EditorUI::GetInstance()
 {
@@ -11,15 +21,66 @@ void EditorUI::Update()
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
-    ImGui::ShowDemoWindow();
+    SetupLayout();
 }
 
-EditorUI::EditorUI()
+void EditorUI::SetupLayout()
 {
-    ImGui::StyleColorsDark();
+    // Setup docking space
+    ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+    {
+        // Setup menu bar
+        if (ImGui::BeginMainMenuBar())
+        {
+            if (ImGui::BeginMenu("File"))
+            {
+                if (ImGui::MenuItem("New"))
+                {
+                    NotImplementedWarning();
+                }
+
+                if (ImGui::MenuItem("Open"))
+                {
+                    NotImplementedWarning();
+                }
+
+                if (ImGui::MenuItem("Save"))
+                {
+                    NotImplementedWarning();
+                }
+
+                if (ImGui::MenuItem("Exit"))
+                {
+                    exit(0);
+                }
+
+                ImGui::EndMenu();
+            }
+
+            ImGui::EndMainMenuBar();
+        }
+
+        // Setup editor panel
+        ImGui::Begin("Editor Tools", nullptr);
+        {
+
+        }
+        ImGui::End();
+
+        // Render window
+        ImGui::Begin("Render", nullptr);
+        {
+
+        }
+        ImGui::End();
+
+        // Setup debug log
+        ImGui::ShowDebugLogWindow();
+    }
+    ImGui::End();
 }
 
-EditorUI::~EditorUI()
+void EditorUI::NotImplementedWarning()
 {
-
+    EXPECT_ERROR(1, "This functionality is not implemented");
 }
--- Sources/Editor/EditorUI.h
+++ Sources/Editor/EditorUI.h
@@ -11,4 +11,6 @@ public:
 private:
     EditorUI();
     ~EditorUI();
+    void SetupLayout();
+    void NotImplementedWarning();
 };
